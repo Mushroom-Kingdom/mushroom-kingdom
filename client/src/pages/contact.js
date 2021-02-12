f5a39c906720fa50c7dc42d7aae46a1fe2a878aa
Linh Nguyen <linhnhatmen@gmail.com>
2021-02-12 12:24:25 +0700

    Adding hook to handle form submit in contact page

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css/contact.css';
 import { Button, Form } from 'react-bootstrap';
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   function HandleSubmit(e){
     e.preventDefault();
-    console.log("testing")
+    console.log(name);
+    console.log(email);
+    console.log(message);
   }
 
   return (
@@ -13,15 +19,26 @@ function Contact() {
           <Form onSubmit={HandleSubmit}>
             <Form.Group controlId="Name">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="Name" placeholder="Eg. John" />
+              <Form.Control 
+                type="Name" 
+                placeholder="Eg. John" 
+                onChange={e => setName(e.target.value) }
+              />
             </Form.Group>
             <Form.Group controlId="Email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="Email" placeholder="email@example.com" />
+              <Form.Control 
+                type="Email" 
+                placeholder="email@example.com" 
+                onChange={e => setEmail(e.target.value) }
+              />
             </Form.Group>
             <Form.Group controlId="Message">
               <Form.Label>Message:</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as="textarea" 
+                rows={3} 
+                onChange={e => setMessage(e.target.value)}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">Submit</Button>
           </Form>
