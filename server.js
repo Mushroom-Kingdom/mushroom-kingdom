4829b472dafb5727e6b43e407d3dc32c39b7db76
Selma Elgabalawy <salmosha88@gmail.com>
2021-02-13 13:40:31 -0800

    fixing dependency issues

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const path = require("path");
 const dotenv = require("dotenv").config();
 
@@ -18,7 +17,7 @@ app.use(
 app.use(express.json());
 
 app.use(express.static("public"));
-require("./routes/api/books")(app);
+// require("./routes/api/books")(app);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -28,23 +27,29 @@ if (process.env.NODE_ENV === "production") {
 // Send every request to the React app
 // Define any API routes before this runs
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Connect to database and then launch the webserver
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tudpv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(result => {
-  console.log("Connected to database");
-  app.listen(PORT, function() {
-    console.log(`🌎 ==> Server is now running on port ${PORT}!`);
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tudpv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then((result) => {
+    console.log("Connected to database");
+    app.listen(PORT, function () {
+      console.log(`🌎 ==> Server is now running on port ${PORT}!`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-}).catch(err => {
-  console.log(err);
-});
-
