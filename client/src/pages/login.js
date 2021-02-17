802ae4e1ee4d0677349da12f578ecc37fbbc61d6
Linh Nguyen <linhnhatmen@gmail.com>
2021-02-17 10:41:06 +0700

    update

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,39 +1,75 @@
-import React, {useState} from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import AuthenticationContext from "../contexts/AuthenticationContext";
+import API from "../utils/API";
 import './css/login.css';
+
 
 function Login (){
     //Authentication system requires here
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = useContext(AuthenticationContext);
+
+    async function loginSubmitHandler(event) {
+      event.preventDefault();
+
+      const userCredentials = {
+        email: email,
+        password: password
+      }
+
+      try {
+
+        let response = await API.loginUser(userCredentials);
+        if(response.status !== 201) {
+            throw new Error(response.message);
+        }
+
+        auth.login(response.data.userID, response.data.isAdmin, response.data.token);
+
+      }catch(err){
+        
+        console.error(err);
+      }
+    }
 
     return(
         <div className="login general-form">
-        <Form>
+          <h1 className="text-center mb-4">Log In</h1>
+        <Form onSubmit={loginSubmitHandler}>
             <Form.Group controlId="Email">
               <Form.Label>Email</Form.Label>
               <Form.Control 
                 type="Email" 
                 placeholder="email@example.com" 
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                required={true}
               />
             </Form.Group>
             <Form.Group controlId="Password">
-              <Form.Label>password</Form.Label>
+              <Form.Label>Password</Form.Label>
               <Form.Control
                 type="Password"  
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+                required={true}
               />
             </Form.Group>
-            <Form.Group controlId="remember-me">
+            {/*<Form.Group controlId="remember-me">
                 <Form.Check 
                     type="checkbox"
                     label="Remember Me"
                 />
-            </Form.Group>
+              </Form.Group>*/}
             <Button variant="primary" type="submit" block>Submit</Button>
             <div style={{overflow:"hidden"}}>
                 <p className="forgot-password text-left">
                         Don't have an <a href="/signup"> account?</a>
                 </p>
                 <p className="forgot-password text-right">
-                        Forgot <a href="/forgot-password"> password?</a>
+                        Forgot your <a href="/forgot-password"> password?</a>
                 </p>
             </div>
           </Form>
