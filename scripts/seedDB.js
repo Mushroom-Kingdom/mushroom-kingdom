42a7e19c4abd04ae5cf9c16ebb34f0bf51fdb9aa
Scott Nelson <snelson.seattle@gmail.com>
2021-02-16 21:50:54 -0800

    fixed export in AdminProducts.js react page

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,52 +1,45 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const db = require("../models");
 
 // need to update localhost address
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tudpv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+  process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
-const mushroomSeed = [
+const mushroomSeed = [  
   {
-    mushroom: [
-      {
-
-        name: " ",          //name of mushroom
-        image_url: " ",     //image of mushroom 
-        description: " ",   //description of mushroom
-        family: " ",        //family of mushroom
-        genus: " ",         //genus of mushroom
-        species: " ",       //species of mushroom
-        price: " ",         //price of mushroom -> integer
-        available: " ",     //quantity of -> boolean true / false
-        toxic: " "          //toxic of mushroom -> boolean true / false
-
-      },
-      {
-
-        name: " ",          //name of mushroom
-        image_url: " ",     //image of mushroom 
-        description: " ",   //description of mushroom
-        family: " ",        //family of mushroom
-        genus: " ",         //genus of mushroom
-        species: " ",       //species of mushroom
-        price: " ",         //price of mushroom -> integer
-        available: " ",     //quantity of -> boolean true / false
-        toxic: " "          //toxic of mushroom -> boolean true / false
-
-      }
-    ]
+    name: "White Mushroom",          //name of mushroom
+    image_url: "whitemushroom.jpg",     //image of mushroom 
+    description: "A white mushroom",   //description of mushroom
+    family: "Mushroom Family",        //family of mushroom
+    genus: "Mushroom Genus",         //genus of mushroom
+    species: "Mushroom Species",       //species of mushroom
+    price: 10,         //price of mushroom -> integer
+    available: true,     //quantity of -> boolean true / false
+    toxic: false          //toxic of mushroom -> boolean true / false
   },
+  {
+    name: "Brown Mushrom",          //name of mushroom
+    image_url: "brownmushroom.jpg",     //image of mushroom 
+    description: "A brown mushroom",   //description of mushroom
+    family: "Mushroom family",        //family of mushroom
+    genus: "Mushroom Genus",         //genus of mushroom
+    species: "Mushroom Species",       //species of mushroom
+    price: 10,         //price of mushroom -> integer
+    available: true,     //quantity of -> boolean true / false
+    toxic: false          //toxic of mushroom -> boolean true / false
+  }
 ];
 
-db.mushroom.remove({})
-  .then(() => db.mushroom.collection.insertMany(mushroomSeed))
+db.Mushroom.remove({})
+  .then(() => db.Mushroom.collection.insertMany(mushroomSeed))
   .then((data) => {
-    console.log(data.result.n + "sale");
+    console.log(data);
     process.exit(0);
   })
   .catch((err) => {
