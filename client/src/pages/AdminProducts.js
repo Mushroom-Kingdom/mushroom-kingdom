d8262a87bc0327ead33aba8a8d713d897c29462f
Paul Besliu <besliu_marius@yahoo.com>
2021-02-18 19:12:17 -0800

    deploying

--- client/src/pages/AdminProducts.js
+++ client/src/pages/AdminProducts.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function AdminDashboard() {    
+function AdminProducts() {    
 
     return (            
         <section>
@@ -9,4 +9,4 @@ function AdminDashboard() {
     )
 }
 
-export default AdminDashboard;
+export default AdminProducts;
