c8f9a464ad8404e7b7e64e3125c395d18549188d
Linh Nguyen <linhnhatmen@gmail.com>
2021-02-20 22:57:48 +0700

    more changes to order form

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -18,16 +18,6 @@ function Order(){
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="name@example.com" />
             </Form.Group>
-            <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Label>Example select</Form.Label>
-                <Form.Control as="select">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                </Form.Control>
-            </Form.Group>
             <Form.Group controlId="MushroomList">
                 <Form.Label>List of mushrooms available:</Form.Label>
                 <Form.Control as="select" multiple>
