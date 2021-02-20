8244d7b98ed3c5e974abac7a0c73bacb5f3c6d1b
Paul Besliu <besliu_marius@yahoo.com>
2021-02-19 19:52:02 -0800

    modal react route

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -2,50 +2,83 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import './css/about.css';
 import FullList from './FullList.js';
+
+import API from "../utils/API"; //just add
+
+
 // import EditModal from '../components/Modal/EditModal.js';
 // import { Button } from 'react-bootstrap';
 var image1 = require('./images/blueOyster.jpg');
+
+
 
 
 function About() {
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
+    // just add 
+    const [mushroomObject, setMushroomObject] = useState({});
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setMushroomObject({ ...mushroomObject, [name]: value })
+
+    }
+    function deleteMushroom(id) {
+        API.deleteMushroom(id)
+            .then(res => )
+            .catch(err => console.log(err));
+    }
 
 
+    function handleMushroomSubmit(event) {
+        event.preventDefault();
+        API.saveMushroom({
+            name: mushroomObject.name,
+            description: mushroomObject.description,
+            family: mushroomObject.family,
+            genus: mushroomObject.genus,
+            species: mushroomObject.species,
+            toxic: mushroomObject.toxic
+        }).then(res => )
+            .catch(err => console.log(err));
+    }
+    // until this line
+    
     return (
-        
+
         <div className="about">
             <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
 
             <Modal isOpen={modalIsOpen}>
                 <h3>Name of mushroom to go here</h3>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <input placeholder="Name"></input>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <textArea id="desc" placeholder="Description"></textArea>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <input placeholder="Toxicity"></input>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <input placeholder="Family"></input>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <input placeholder="Genus"></input>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <input placeholder="Species"></input>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <button onClick={() => setModalIsOpen(false)}>Close Modal</button>
                 <button onClick={() => setModalIsOpen(false)}>Save Changes</button>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <FullList />
-                
+
             </Modal>
             <div id="header">
                 <h1>OUR MISSION</h1>
@@ -61,21 +94,21 @@ function About() {
             <div id="section">
                 <h1>History</h1>
                 <p>
-                    The Mushroom Kingdom has modest beginnings with two friends from high school, Nick Pasch and Bryce Van Note, searching for their calling in a pandemic-stricken world. Nick had just dropped out of college and Bryce had just graduated high school, and the two were eager to begin working. What ensued was a period of informal research and experimentation with many different avenues of interest, one of which being mushrooms. After growing as hobbyists for some months and acquiring vast knowledge and experience growing mushrooms, we decided to begin this mushroom farm. Now we face the challenge of producing the highest quality fungus for affordable rates, an obstacle we hope to overcome with evermore success. 
+                    The Mushroom Kingdom has modest beginnings with two friends from high school, Nick Pasch and Bryce Van Note, searching for their calling in a pandemic-stricken world. Nick had just dropped out of college and Bryce had just graduated high school, and the two were eager to begin working. What ensued was a period of informal research and experimentation with many different avenues of interest, one of which being mushrooms. After growing as hobbyists for some months and acquiring vast knowledge and experience growing mushrooms, we decided to begin this mushroom farm. Now we face the challenge of producing the highest quality fungus for affordable rates, an obstacle we hope to overcome with evermore success.
                 </p>
-                <br/>
+                <br />
                 <h1>How We Grow</h1>
                 <p>
-                    Our entirely organic mushroom farm operates using techniques that were popularized in the past few decades. We begin by using organic Wild Bird Seed, which we rinse clean and let soak for a period of 24 hours. Once this grain had soaked, we sterilize the grain by placing it into jars and pressure cooking it for 2 hours. Once this is complete and the jars have cooled off, we transfer mycelium into the sterile jar in a sterile working environment produced by an air filtration system we built called a laminar flow hood. Once a jar has had mycelium transferred into it, the mycelium grows to occupy the entire jar over a period of about 2 weeks. Once it has fully colonized the jar, it can be used to produce more jars or combined with pasteurized straw to begin fruiting in our self-designed fruiting chamber. Given the optimal conditions that our fruiting room has been designed to maintain, we fruit beautiful mushrooms roughly 4 weeks after the start of the process. 
+                    Our entirely organic mushroom farm operates using techniques that were popularized in the past few decades. We begin by using organic Wild Bird Seed, which we rinse clean and let soak for a period of 24 hours. Once this grain had soaked, we sterilize the grain by placing it into jars and pressure cooking it for 2 hours. Once this is complete and the jars have cooled off, we transfer mycelium into the sterile jar in a sterile working environment produced by an air filtration system we built called a laminar flow hood. Once a jar has had mycelium transferred into it, the mycelium grows to occupy the entire jar over a period of about 2 weeks. Once it has fully colonized the jar, it can be used to produce more jars or combined with pasteurized straw to begin fruiting in our self-designed fruiting chamber. Given the optimal conditions that our fruiting room has been designed to maintain, we fruit beautiful mushrooms roughly 4 weeks after the start of the process.
                 </p>
-                <br/>
+                <br />
                 <h1>Section</h1>
                 <p>
-                    
+
                 </p>
             </div>
         </div>
-    
+
     )
 }
 
